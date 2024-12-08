2d0c3d090c2a595b5bd6a4257ab0aeb88050fdf3
AIS\8wuby <8wubyq35@gmail.com>
2024-12-08 17:53:37 +0800

    1.0.5

--- linux-aisio.sh
+++ linux-aisio.sh
@@ -2,24 +2,21 @@
 
 Usage() {
     echo "Usage: $0 <operation> [-f <filename>] [-n <iterations>]"
-    echo "Available operations: -w (write), -a (append), -i (insert), -r (read), -rl (read-all), -x (indexes), -d (remove)"
+    echo "Available operations: -w (write), -a (append), -i (insert), -r (read-all), -id (indexes), -rm (remove), -rs (remove-index)"
     exit 1
 }
 
-# 默認
 file="test.bin"
 iterations=1
 
-# 檢查參數是否被提供
 if [ "$#" -eq 0 ]; then
     Usage
 fi
 
-# 解析參數
 operation=""
 while [[ "$#" -gt 0 ]]; do
     case "$1" in
-        -w|-a|-i|-r|-rl|-x|-d)
+        -w|-a|-i|-r|-id|-rm|-rs)
             operation="$1"
             shift
             ;;
@@ -36,14 +33,13 @@ while [[ "$#" -gt 0 ]]; do
             ;;
     esac
 done
+index_list=$(seq 0 $((iterations-1)) | tr '\n' ' ')
 
-# 檢查參數是否是指定的操作
 if [ -z "$operation" ]; then
     echo "Error: Operation is required."
     Usage
 fi
 
-# 執行操作多次
 for ((i=1; i<=iterations; i++)); do
     echo "Iteration $i/$iterations"
     case "$operation" in
@@ -60,21 +56,22 @@ for ((i=1; i<=iterations; i++)); do
             ./aisio --insert "$file" -bool true 0 -byte 255 0 -sbyte -128 0 -short 32767 0 -ushort 65535 0 -int 2147483647 0 -uint 4294967295 0 -long 9223372036854775807 0 -ulong 18446744073709551615 0 -float 3.1415927 0 -double 3.141592653589793 0 -bytes "This is Ais.IO Function Byte Array." 0 -string "This is Ais.IO Function String." 0
             ;;
         -r)
-            echo "Ais Binary IO Read..."
-            ./aisio --read "$file" -bool -byte -sbyte -short -ushort -int -uint -long -ulong -float -double -bytes -string
-            ;;
-        -rl)
             echo "Ais Binary IO Read all..."
             ./aisio --read-all "$file"
             ;;
-        -x)
+        -id)
             echo "Ais Binary IO Indexes..."
             ./aisio --indexes "$file"
             ;;
-        -d)
+        -rm)
             echo "Ais Binary IO Remove..."
             ./aisio --remove "$file" -string 0 32
             ;;
+	-rs)
+	    echo "Ais Binary IO Remove Index..."
+	    ./aisio --remove-index "$file" $index_list
+	    break
+	    ;;
         *)
             Usage
             ;;
